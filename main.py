9821238c79d114074f58c72e88acf4f1b9ad39e2
blackenergy2000 <78444120+blackenergy2000@users.noreply.github.com>
2021-02-02 21:01:05 +0100

    Test

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 # This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def find_bot():
+    pathfinder = os.listdir("D:/Bot/")
+    print(pathfinder[0])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def say_hello():
+    print("Hello")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+find_Bot()
