495d8577c3b0a09bbbc121b6ee987ceed909c234
blackenergy2000 <78444120+blackenergy2000@users.noreply.github.com>
2021-02-02 23:14:05 +0100

    Test audio_file

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 # This is a sample Python script.
 import os
+import stat
+from playsound import playsound
+from discord import FFmpegPCMAudio, PCMVolumeTransformer
+
+def look_for_direc():
+    path = "D:/Bot/"
+    if os.path.isdir(path):
+        print("vorhanden")
+        pathfinder = os.listdir("D:/Bot/")
+        for i in range(len(pathfinder)):
+            print(pathfinder[i])
+    else:
+        os.mkdir(path)
+        os.chmod(path,  stat.S_IRWXO)
 
 
-def find_bot():
-    pathfinder = os.listdir("D:/Bot/")
-    print(pathfinder[0])
+def play_sounds(path):
+    playsound(path + "SPEICHER.mp3")
 
 
-def say_hello():
-    print("Hello")
-
-
-find_bot()
+play_sounds("D:/Bot/")
