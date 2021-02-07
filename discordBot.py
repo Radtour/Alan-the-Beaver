51e221b013b86a4dd2e3317ed1c50cac45e220e5
blackenergy2000 <78444120+blackenergy2000@users.noreply.github.com>
2021-02-07 22:56:56 +0100

    bot.command.playfile

--- discordBot.py
+++ discordBot.py
@@ -1,21 +1,45 @@
 import discord
 import random
 from discord.ext import commands
+import asyncio
 
 #client = discord.Client()
-client = commands.Bot(command_prefix='!')
-
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
-@client.command
+@client.command()
 async def test(ctx):
-    print("Test")
-    await ctx.send("Test")
+    await ctx.send('Test')
 
+@client.command()
+async def play(ctx, file_path):
+    await ctx.send("Folgende Audio-Datei wird abgespielt: " + file_path)
+
+@client.command()
+async def vuvuzela(ctx):
+    # grab the user who sent the command
+    user = ctx.message.author
+    voice_channel = user.voice.voice_channel
+    channel = None
+    # only play music if user is in a voice channel
+    if voice_channel != None:
+        # grab user's voice channel
+        channel = voice_channel.name
+        await client.say('User is in channel: ' + channel)
+        # create StreamPlayer
+        vc = await client.join_voice_channel(voice_channel)
+        player = vc.create_ffmpeg_player('SPEICHER.mp3', after=lambda: print('done'))
+        player.start()
+        while not player.is_done():
+            await asyncio.sleep(1)
+        # disconnect after the player has finished
+        player.stop()
+        await vc.disconnect()
+    else:
+        await client.say('User is not in a channel.')
 
 @client.event
 async def on_message(message):
@@ -24,7 +48,6 @@ async def on_message(message):
         return
 
     chatMessage = message.content
-   # check_for_audio(chatMessage)
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
@@ -32,7 +55,7 @@ async def on_message(message):
     if chatMessage.__contains__(":peepoClown:"):
         await message.channel.send("<:peepoClown:806233172564115467>")
 
+    await client.process_commands(message)
 
-#def check_for_audio(message):
-    #if message.content.startswith('!play'):
-        # Here it should be looked for the requested audio
+
+
